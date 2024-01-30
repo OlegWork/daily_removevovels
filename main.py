2c38371e3368d037519ec2879cc5e5b3a485400a
OlegWork <zeus.set@gmail.com>
2024-01-30 22:59:00 +0200

    full program

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello world")
+def disemvowel_trolls(sentence: str) -> str:
+    # just starting to remember
+    vowels = "aeiouAEIOU"
+    new_word = ""
+    for i in range(len(sentence)):
+        if sentence[i] not in vowels:
+            new_word += sentence[i]
+
+    return new_word
+
+
+print(disemvowel_trolls("Delete all vowels!"))
